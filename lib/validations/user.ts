60d9342ab3c750dddfddcc698f0cae621007361b
IvanDanyliuk <ivandaniliuk@gmail.com>
2023-08-07 19:34:21 +0300

    Add server logic: update user and create thread

--- lib/validations/user.ts
+++ lib/validations/user.ts
@@ -2,7 +2,16 @@ import * as zod from 'zod';
 
 export const UserValidation = zod.object({
   profile_photo: zod.string().url().nonempty(),
-  name: zod.string().min(3).max(30),
-  username: zod.string().min(3).max(30),
-  bio: zod.string().min(3).max(1000),
+  name: zod
+    .string()
+    .min(3, { message: 'Minimum 3 characters.' })
+    .max(30, { message: 'Maximum 30 caracters.' }),
+  username: zod
+    .string()
+    .min(3, { message: 'Minimum 3 characters.' })
+    .max(30, { message: 'Maximum 30 caracters.' }),
+  bio: zod
+    .string()
+    .min(3, { message: 'Minimum 3 characters.' })
+    .max(1000, { message: 'Maximum 1000 caracters.' }),
 });
